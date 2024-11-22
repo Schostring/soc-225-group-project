b9672f8e668ee45ccfd5fe73faf6a25ec5645904
Shelby Ames <shelbyames10@gmail.com>
2024-11-22 14:59:20 -0800

    Added histograms with GDP categories and average species labels

--- Autumn 2024/Soc225/GroupProject.R
+++ Autumn 2024/Soc225/GroupProject.R
@@ -20,7 +20,7 @@ filtered_merge <- merged_data %>%
   filter(Year == "2017")
 
 filtered_merge2 <- filtered_merge %>%
-  filter(!is.na(Carbon_Value))
+  filter(!is.na(Carbon_Value), Series.x == "Threatened Species: Total (number)")
 
 
 plot_unzoomed <- ggplot(filtered_merge2, aes(x = Carbon_Value, y = Species_Value)) +
@@ -66,4 +66,49 @@ lm_plot <- plot(lm_model)
 # Close the device to save the file
 dev.off()
 
+gdp <- read_csv("Autumn 2024/Soc225/UNdata_Export_20241122_154003154.csv")
 
+gdp = rename(gdp, Country = `Country or Area`)
+gdp = rename(gdp, GDP_Value = Value)
+
+new_merge <- left_join(gdp, filtered_merge2, by = "Country")
+
+new_merge <- new_merge %>%
+  mutate(Prosperity_Category = case_when(
+    GDP_Value <= 1135 ~ "Low prosperity",
+    GDP_Value > 1135 & GDP_Value <= 4465 ~ "Lower-Mid prosperity",
+    GDP_Value > 4465 & GDP_Value <= 13845 ~ "High-Mid prosperity",
+    GDP_Value > 13845 ~ "High prosperity",
+    TRUE ~ NA_character_  
+  ))
+
+
+avg_by_category <- new_merge %>%
+  group_by(Prosperity_Category) %>%
+  summarise(avg_species_value = mean(Species_Value, na.rm = TRUE))
+
+# Plot with vertical lines at the average for each GDP category
+gdp_cat_species2 <- ggplot(new_merge, aes(x = Species_Value, fill = Prosperity_Category)) +
+  geom_histogram() +  # Create histogram for Threatened Species
+  facet_wrap(~ Prosperity_Category) +  # Facet by Prosperity Category with free y scales
+  labs(
+    title = "Threatened Species Across GDP Categories",
+    x = "Number of Threatened Species",
+    y = "Number of Countries",
+    fill = "Prosperity Category"
+  ) +
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 5)) +  # Adjust y-axis breaks
+  theme_minimal() +
+  theme(legend.position = "none") +
+  xlim(0, 800) +
+  geom_vline(data = avg_by_category, aes(xintercept = avg_species_value), 
+             color = "red", linetype = "dashed", linewidth = 1) +  # Add vertical line for averages
+  geom_text(data = avg_by_category, aes(x = avg_species_value+200, y = 7, 
+                                        label = paste("Avg: ", round(avg_species_value, 0))),
+            color = "red", fontface = "italic", size = 4, vjust = 1)
+
+gdp_cat_species2
+ggsave("species_gdp.png", plot = gdp_cat_species2, width = 8, height = 6,dpi=300)
+
+lm_model2 <- lm(Species_Value ~ GDP_Value, data = new_merge)
+summary(lm_model2)
